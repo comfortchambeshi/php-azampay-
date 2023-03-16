316945b90322aa62f12063579c5eb2cb474a42a1
comfortchambeshi <comfortchambeshi4@gmail.com>
2023-03-16 11:30:57 +0200

    modified:   classes/azampay.php

--- classes/azampay.php
+++ classes/azampay.php
@@ -6,7 +6,7 @@ class azampay
   private static $environment = "sandbox";
 	private static $appName = "Witlevels";
 	private static $clientId = "51bb0d77-c007-4eb4-b181-542b73c41124";
-	private static $clientSecret = "HdwG9OzKv7G+IVxua2DjFZJoGj90Ii6RUcOYG+Z2gVyzZYXjtzle4+db6IXwcISvvODpXc8DoQPu6xN8qifWjuJ2qLblISihZ6QAzsqEe8VtQcT+jzg4aPW14sM1RQai/l1FUzj1sMy569QNaYltKxILT32k/As5IX8oVNH+tVC6Ll2s/XSj6Db9vTQMJavo+eWOc1Aeb9etne2lJTDXm23HzAxWek+U2Jg/o1H0kGO/l3KEG2D7hJ9A3wp2QAoUm/2mlYIRA+CKa+ADN6QAsxZScrV7Wwfg66DnpG2BQpgDa7nIWI75mSEFRlaBq3OqQPSbEo9HROuleU8TwTHMF1btciVNYv3AchNZggcmS9jPuFzF9h68oU7hZwH1/sVEOYc6IyBrX2hNm55iVOFK3Dne3S9z1erZVfrlSffDXoaD4jNCdaqBV/ObkSrsEWmThQqVWrwib6nI7r4z/ruCEgy+rL51COiTUpdqncIk2GbNx6uj07lZdHqKLry3EocGAX+hq4VVoWovKHiiuwEfBUOQachBt4RtmqvArUmxhwHSCnUoE+nTYT63XHlwFFxp7gfcashgVmKd4SbK734LxaKY6jbfXN5haHLExmsuCEY0NXPhYWXOXqttZgTJTLKhznJnktzhgHzLbE1kZokbf9YfUCJ4rWsryvni2ORcWPU=";
+	private static $clientSecret = "IJtnxdiR5qN91qZVoJh46/R4DdV+p/A6L76uphFWnbwIEUvoKYpQNy7aobH6T05jQJX48GDAo16uPbEfequaugCp+w1fXyMMINxaiMNwmM0v1n0pWiqkjD+doW+UzSGyGB+42PnT2pnK743xXCBb8loCMJuqZtYtC0nbiGN1zW+1Fhh8Y+Q+g8j4Uh9tPfDUz12eqjo4TLtd2Xnc6FnonvsgQpDOEBpVPhoX3htY3Ijj/3E/87YPNPDEhA4ZE4aMWPxiwwzqyq5eRjitfut5C2mbQnSIfa2mvHat2j2G4ljZ2pOClIrW5G/1/0dZc/QHUFLceanrPBuHsox+cq8CcPflZxWg8pPWuqCqM6OhujJ8AIQqs8hyV20tB0Y8N5RQXIT1h1CgwpA1HF55NyCq3C1zYKamLNWtvehHsdLlCAMP0M8v9nZrRi/Q/Y4/RdFYJOzKb9K2Sx3wG5Ocgcjb+FRLcLHuQ1QIh5Ec3OHzfi4coPuGz0qeNfxABV3tkglBxZLvrtXJKZsZpZql5mL7InZprxNY9Zp4my8qvvtTOP2ILxFdWl33nmaXm5RZfsAatABKjdV9XhHhj0GiZi/5Q11hgUbmk1w/YiS+EB+Tz8eASjkxL9rAcNoVBIM8G+KnOj/ptqEL/doijJZ885HL4YUGutIfHU8PtrBmf47qkSk=";
 
 	//Environment URLS
 	public static function envUrls()
@@ -15,7 +15,7 @@ class azampay
 		$checkout_url = "https://sandbox.azampay.co.tz";
         
         //Base URLs for production
-		if (azampay::$environment == "production") {
+		if (AzamPay::$environment == "production") {
 			$auth_url = "https://authenticator.azampay.co.tz";
 		    $checkout_url = "https://checkout.azampay.co.tz";
 			
@@ -41,9 +41,9 @@ class azampay
               CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
               CURLOPT_CUSTOMREQUEST => 'POST',
               CURLOPT_POSTFIELDS =>'{
-              "appName": "'.azampay::$appName.'",
-              "clientId": "'.azampay::$clientId.'",
-              "clientSecret": "'.azampay::$clientSecret.'"
+              "appName": "'.AzamPay::$appName.'",
+              "clientId": "'.AzamPay::$clientId.'",
+              "clientSecret": "'.AzamPay::$clientSecret.'"
               }',
                 CURLOPT_HTTPHEADER => array(
                   'Content-Type: application/json'
@@ -64,7 +64,7 @@ class azampay
              $externalId = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex(random_bytes(16)), 4));
 		      $curl = curl_init();
               curl_setopt_array($curl, array(
-                CURLOPT_URL => ''.azampay::envUrls()["checkout_url"].'/azampay/mno/checkout',
+                CURLOPT_URL => ''.AzamPay::envUrls()["checkout_url"].'/azampay/mno/checkout',
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_ENCODING => '',
                 CURLOPT_MAXREDIRS => 10,
@@ -84,7 +84,7 @@ class azampay
                 "provider": "'.$provider.'"
               }',
                 CURLOPT_HTTPHEADER => array(
-                  'Authorization: Bearer '.azampay::authtoken()->data->accessToken.'',
+                  'Authorization: Bearer '.AzamPay::authtoken()->data->accessToken.'',
                   'Content-Type: application/json'
                 ),
               ));
@@ -97,4 +97,3 @@ class azampay
                return array($result, $externalId);
         }
 	}
-
